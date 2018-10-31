a2afb32415ed8fe7d64e2ca0e63383d08ecd3194
Artsemi Dzmitryieu <Artsemi_Dzmitryieu@epam.com>
2018-10-31 08:52:17 +0300

    Add author's name

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ def hello():
     return "Time is: " + time.asctime(time.localtime(time.time()))
 
 
+@app.route('/author')
+def author():
+    return "Author's name is Artsemi Dzmitryieu"
+
+
 if __name__ == '__main__':
     app.run()
